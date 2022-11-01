3cf4115631583910c2747005f19cce020ba30a76
dvimukthi <deshanvimukthi59@gmail.com>
2022-11-01 23:06:16 +0530

    title

--- SnakeGame.py
+++ SnakeGame.py
@@ -12,21 +12,31 @@ FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR = "#000000"
 
 
-
 class Snake:
     pass
+
 
 class Food:
     pass
 
+
 def next_turn():
     pass
+
 
 def change_direction(new_direction):
     pass
 
+
 def check_collisions():
     pass
 
+
 def game_over():
     pass
+
+
+window = Tk()
+window.title("Snake Game")
+
+window.mainloop()
