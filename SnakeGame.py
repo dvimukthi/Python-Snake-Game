b70fc90e220675c56e0f37ff06b1238fe1dc8670
dvimukthi <deshanvimukthi59@gmail.com>
2022-11-02 22:51:51 +0530

    geometry

--- SnakeGame.py
+++ SnakeGame.py
@@ -2,6 +2,7 @@ from asyncio import windows_events
 from cProfile import label
 from tkinter import *
 import random
+from turtle import window_height, window_width
 
 
 GAME_WIDTH = 700
@@ -54,5 +55,16 @@ canvas = Canvas(window, bg=BACKGROUND_COLOR,
                 height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
 
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
 window.mainloop()
